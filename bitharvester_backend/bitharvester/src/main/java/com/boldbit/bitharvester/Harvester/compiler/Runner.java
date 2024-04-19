a4d1a0dc6ff4bc1df7cbf6c0c5108450b333d395
Suresh <jaisehrawat11@gmail.com>
2024-04-19 12:05:22 +0530

    Runner class optimization

--- bitharvester_backend/bitharvester/src/main/java/com/boldbit/bitharvester/Harvester/compiler/Runner.java
+++ bitharvester_backend/bitharvester/src/main/java/com/boldbit/bitharvester/Harvester/compiler/Runner.java
@@ -3,46 +3,40 @@ package com.boldbit.bitharvester.Harvester.compiler;
 import java.io.FileWriter;
 
 import com.boldbit.bitharvester.Harvester.compiler.Analyzer.Program;
-import com.boldbit.bitharvester.Harvester.compiler.Analyzer.ProgramData;
 import com.boldbit.bitharvester.Harvester.compiler.parser.Parser;
 import com.boldbit.bitharvester.Harvester.compiler.source.StaticSourceFile;
 import com.boldbit.bitharvester.Harvester.compiler.token.SourceFile;
-import com.boldbit.bitharvester.Harvester.compiler.trees.ProgramTree;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Runner {
 
     public static void parse(StaticSourceFile staticSourceFile) {
-        SourceFile sourceFile = new SourceFile(staticSourceFile.fileName, staticSourceFile.sourceString,
-                staticSourceFile.fileSize);
         try {
+            SourceFile sourceFile = new SourceFile(staticSourceFile.fileName, staticSourceFile.sourceString,
+                    staticSourceFile.fileSize);
+
             Parser parser = new Parser(sourceFile);
             Program program = parser.parseProgram();
-            ProgramData programData = program.programData;
-            ProgramTree programTree = program.programTree;
 
             ObjectMapper objectMapper = new ObjectMapper();
-            String programDataJsonString = objectMapper.writeValueAsString(programData);
-            String programTreeJsonString = objectMapper.writeValueAsString(programTree);
 
-            String programDataFilePath = "bitharvester_backend/bitharvester/src/main/java/com/boldbit/bitharvester/Harvester/compiler/doc/programData.json";
-            String programTreeFilePath = "bitharvester_backend/bitharvester/src/main/java/com/boldbit/bitharvester/Harvester/compiler/doc/programTree.json";
-
-            FileWriter fileWriter;
-
-            fileWriter = new FileWriter(programDataFilePath);
-            fileWriter.write(programDataJsonString);
-            fileWriter.close();
-
-            fileWriter = new FileWriter(programTreeFilePath);
-            fileWriter.write(programTreeJsonString);
-            fileWriter.close();
-
+            writeJsonToFile("programData", objectMapper.writeValueAsString(program.programData));
+            writeJsonToFile("programTree", objectMapper.writeValueAsString(program.programTree));
+            
             System.out.println("JSON saved!!!");
-
         } catch (Exception e) {
-            System.out.println("Exception in parsing");
-            System.out.println("Exception:- " + e);
+            System.out.println("Exception in parsing:- " + e);
+        }
+    }
+
+    private static void writeJsonToFile(String fileName, String jsonString) {
+        String filePath = "bitharvester_backend/bitharvester/src/main/java/com/boldbit/bitharvester/Harvester/compiler/doc/"
+                + fileName + ".json";
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            fileWriter.write(jsonString);
+            fileWriter.close();
+        } catch (Exception e) {
+            System.out.println("Exception in Json writing:- " + e);
         }
     }
 }
